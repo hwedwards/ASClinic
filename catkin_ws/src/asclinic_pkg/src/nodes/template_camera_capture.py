21629d6bb411ef6ac7ed7e3574ba98651517bcc0
Paul Beuchat <paul.beuchat@unimelb.edu.au>
2021-05-03 09:05:22 +1000

    Added template node for detecting aruco markers within a a camera image. Also adjusted to template_camera_capture node to save the camera image anytime OpenCV detects a calibration chessboard within the image.

--- catkin_ws/src/asclinic_pkg/src/nodes/template_camera_capture.py
+++ catkin_ws/src/asclinic_pkg/src/nodes/template_camera_capture.py
@@ -106,6 +106,24 @@ class TemplateCameraCapture:
             # Publish the camera frame
             rospy.loginfo('[TEMPLATE CAMERA CAPTURE] Now publishing camera frame')
             self.image_publisher.publish(self.cv_bridge.cv2_to_imgmsg(current_frame))
+
+            # Check if the camera frame contains a calibration
+            # chessboard that can be detected by OpenCV
+            # > Convert the image to gray
+            current_frame_as_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+            # > Specify the flags for the find chessboard function
+            find_flags = cv2.CALIB_CB_ADAPTIVE_THRESH #+ cv2.CALIB_CB_NORMALIZE_IMAGE + cv2.CALIB_CB_FAST_CHECK
+            # > Call the find the chess board corners
+            #   > The second argument is the grid size of internl corner
+            #     points that the function should search for
+            ret, corners = cv2.findChessboardCorners(current_frame_as_gray, (6,5), flags=find_flags)
+            # If found, then set the save image flag to true
+            if ret == True:
+                rospy.loginfo("[TEMPLATE CAMERA CAPTURE] Chessboard FOUND, this image will be saved")
+                self.should_save_image = True
+            #else:
+            #    rospy.loginfo("[TEMPLATE CAMERA CAPTURE] Chessboard NOT found")
+
             # Save the camera frame if requested
             if (self.should_save_image):
                 # Increment the image counter
