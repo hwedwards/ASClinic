d57cc41b5cd272341e5b71f6f7d8801212b29f90
tjzay <zayontzt@gmail.com>
2025-04-05 20:45:07 +1100

    A 'odomtester' node created for testing the robot - requires 'odometer' node to be running - subscribes to the topic 'Pose' and tracks total distance travelled since the node started running - publishes to the topic '/asc/set_motor_duty_cycle' to start the robot moving - after a certain DISTANCE (in mm), the node then publishes to zero duty cycle for both wheels to the topic '/asc/set_motor_duty_cycle' to stop it from moving - note: if you want to reset the pose to zero, kill the 'odometer' node and rerun it

---
+++ catkin_ws/src/asclinic_pkg/src/nodes/odomtester.cpp
@@ -0,0 +1,92 @@
+#include "ros/ros.h"
+#include <ros/package.h>
+#include <math.h>
+#include "asclinic_pkg/PoseSeqs.h"
+#include "asclinic_pkg/LeftRightInt32.h"
+#include "asclinic_pkg/LeftRightFloat32.h"
+
+#define THRESHOLD_DISTANCE 1000 // in mm
+
+// use global variables, static means scope is limited to this script
+static float d = 0, pose_x = 0, pose_y = 0;
+static bool first_message = true; // Flag to handle first message separately
+
+// whenever a message comes to the '/asc/encoder_counts' topic, this callback is executed
+void calculate_distance(const asclinic_pkg::PoseSeqs &msg)
+{
+    // Skip distance calculation for the first message
+    if (first_message)
+    {
+        pose_x = msg.x;
+        pose_y = msg.y;
+        first_message = false;
+        ROS_INFO("Initial pose at: (%f, %f)", pose_x, pose_y);
+        return; // Skip distance calculation on first message
+    }
+
+    // Calculate the distance based on the change in position
+    d += sqrt(pow(msg.x - pose_x, 2) + pow(msg.y - pose_y, 2));
+    pose_x = msg.x;
+    pose_y = msg.y;
+}
+
+int main(int argc, char *argv[])
+{
+    d = 0;
+    pose_x = 0;
+    pose_y = 0;
+
+    // Initialise the node
+    ros::init(argc, argv, "odomtester");
+
+    // Initialise a node handle to the group namespace
+    std::string ns_for_group = ros::this_node::getNamespace();
+    ros::NodeHandle nh_for_group(ns_for_group);
+
+    ros::Rate loop_rate(10);
+
+    // Subscribe to /asc/encoder_counts
+    ros::Subscriber pose_subscriber = nh_for_group.subscribe("Pose", 10, calculate_distance);
+
+    // Initialise a publisher
+    ros::Publisher m_publisher = nh_for_group.advertise<asclinic_pkg::LeftRightFloat32>("/asc/set_motor_duty_cycle", 10);
+    asclinic_pkg::LeftRightFloat32 dutycycle;
+
+    ros::Duration(1).sleep();
+    dutycycle.left = 100;
+    dutycycle.right = 100;
+    dutycycle.seq_num = 0;
+
+    m_publisher.publish(dutycycle);
+    ROS_INFO("Duty cycle = %f, %f, Distance travelled = %f", dutycycle.left, dutycycle.right, d);
+    /* Alternatively if you want to run it for a certain time, get rid of the while loop and do this:
+        // Wait 1 seconds
+        ros::Duration(1).sleep();
+        dutycycle.left = 0;
+        dutycycle.right = 0;
+
+        // Then publish final message
+
+        m_publisher.publish(dutycycle);
+        ROS_INFO("Message = %f, %f, %d", dutycycle.left, dutycycle.right, dutycycle.seq_num);
+    */
+
+    // Spin at a specific rate, 10Hz here
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        ROS_INFO("Node is running, Distance travelled: %f", d);
+        if (d > THRESHOLD_DISTANCE)
+        {
+            dutycycle.left = 0;
+            dutycycle.right = 0;
+            m_publisher.publish(dutycycle);
+            ROS_INFO("Duty cycle = %f, %f, Distance travelled = %f", dutycycle.left, dutycycle.right, d);
+            ROS_INFO("Final pose at: (%f, %f)", pose_x, pose_y);
+            break;
+        }
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
